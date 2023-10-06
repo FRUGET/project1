55d294a71e71b0e59127e7214f04278c80a70f72
Vlad Moroz <nouser.you228@gmail.com>
2023-10-06 13:48:59 +0300

    First commit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print(1234)
+Np = int(input("Np: "))
+No = int(input("No: "))
+T = int(input("T: "))
+Ntp = int(input("Ntp: "))
+M = int(input("M: "))
+O = int(input("O: "))
+K = int(input("K: "))
+d = 52
+Td = 168
+
+D = d - O - K
+t = Td - M
+Ri3 = (Np*D*t)/(T*No*d*Td)
+RinB = ((Np+Ntp)*D*t)/(T*No*d*Td)
+
+print(f"D: {D}, t: {t}, Ri3: {Ri3}, RinB: {RinB}")
