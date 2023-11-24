033433154d2eb5bb734f24a1d4b5266d32a53667
Vlad Moroz <nouser.you228@gmail.com>
2023-11-24 21:11:21 +0200

    First commit

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-Np = int(input("Np: "))
-No = int(input("No: "))
-T = int(input("T: "))
-Ntp = int(input("Ntp: "))
-M = int(input("M: "))
-O = int(input("O: "))
-K = int(input("K: "))
-d = 52
-Td = 168
+import time
+import numpy as np
+import matplotlib.pyplot as plt
 
-D = d - O - K
-t = Td - M
-Ri3 = (Np*D*t)/(T*No*d*Td)
-RinB = ((Np+Ntp)*D*t)/(T*No*d*Td)
+def f(x):
+    return x*x - 5*x + 5
+def df(x):
+    return 2*x - 5
+N = 20
+xx = 0
+lmd = 0.1
 
-print(f"D: {D}, t: {t}, Ri3: {Ri3}, RinB: {RinB}")
+x_plt = np.arange(0, 5.0, 0.1)
+f_plt = [f(x) for x in x_plt]
+
+plt.ion()
+fig, ax = plt.subplots()
+ax.grid(True)
+
+ax.plot(x_plt, f_plt)
+point = ax.scatter (xx, f(xx), c='red')
+
+for i in range(N):
+    xx = xx - lmd*df(xx)
+    point.set_offsets([xx, f(xx)])
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    time.sleep(0.05)
+
+plt.ioff()
+print(xx)
+ax.scatter(xx, f(xx), c='blue')
+plt.show()
